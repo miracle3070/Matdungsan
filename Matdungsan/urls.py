28770595350926ef651b369166bb56bb464b46c7
miracle3070 <miracle3070@naver.com>
2020-08-22 14:37:56 +0900

    'accounts' app added

--- Matdungsan/urls.py
+++ Matdungsan/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from pageapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
-    path('signup/', views.signup, name='signup'),
+    path('accounts/', include('accounts.urls')),
 ]
