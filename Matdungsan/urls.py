0d01042319f65b44315cbcd107cb8b5061425189
miracle3070 <miracle3070@naver.com>
2020-08-22 13:47:43 +0900

    Link Added

--- Matdungsan/urls.py
+++ Matdungsan/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pageapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
 ]
