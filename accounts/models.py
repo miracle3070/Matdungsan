af300b92c9d14be06c179a2a08a6f0664b5e4750
miracle3070 <miracle3070@naver.com>
2020-09-03 16:42:12 +0900

    Added model Profile for connect User one to one

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    gender = models.CharField(max_length=10, blank=False)
+    age_group = models.IntegerField(blank=False)
+    complete_count = models.IntegerField(blank=False)
+    grade = models.CharField(max_length=10, blank=False)
